ca2693cac1e99e7bf38f1d321fde11cfa63d7310
Set Cain <charlie.bronson00@gmail.com>
2017-10-31 08:24:38 -0600

    init

---
+++ hello/Hello.java
@@ -0,0 +1,27 @@
+/**
+ * Program to say hello :)
+ * Author: setcain
+ * @version 1.0.0
+ * Date 30/10/17*/
+
+public class Hello {  // Create class
+   
+    // main method
+    // static - dont need to instance for use it
+    // String[] args - args is the name of the array String[]
+    public static void main(String[] args) {
+
+        final String hi;
+        final String hi1;
+        final String hi2;
+
+        hi = "Hello";
+        hi1 = "Beautiful";
+        hi2 = "World";
+
+        // Lib System.out method println
+        System.out.println("The title is: " + hi + hi1 + hi2);
+
+    }
+
+}
