9b05bca468781ad1a69292660ed2df6f6312ce8d
Set Cain <charlie.bronson00@gmail.com>
2017-11-01 10:20:11 -0600

    add classes for work whit

---
+++ opp_example/FirstClass.java
@@ -0,0 +1,12 @@
+/**
+ * Program to show how to work whit objects in JAVA
+ * Author: Ricardo Veronica Duran 000023129
+ * @version 1.0.1
+ * Date 31/10/17*/
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        
+    }
+}
